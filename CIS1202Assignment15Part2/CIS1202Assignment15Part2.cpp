d3b2dea3951b0bb9131ad782f3844eaed977f429
Kacey Hall <kaceyhall61@gmail.com>
2022-05-02 09:59:30 -0400

    Finished

--- CIS1202Assignment15Part2/CIS1202Assignment15Part2.cpp
+++ CIS1202Assignment15Part2/CIS1202Assignment15Part2.cpp
@@ -5,10 +5,30 @@ CIS 1202
 */
 
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
+template<class N>
+N Half(N x)
+{
+    return x / 2;
+}
+
+int Half(int x)
+{
+    double y = x;
+
+    return round(y / 2);
+}
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << Half(a) << endl;
+    cout << Half(b) << endl;
+    cout << Half(c) << endl;
 }
